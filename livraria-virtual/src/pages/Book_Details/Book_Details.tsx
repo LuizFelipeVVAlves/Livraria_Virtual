7b88e393d4d6022cb0816d34fde6dc770c8ca216
LuizFelipeVVAlves <lfvilaverde@gmail.com>
2025-03-02 19:20:54 -0300

    NOVIDADE
    - Implementação da tela dos detalhes do livro

--- livraria-virtual/src/pages/Book_Details/Book_Details.tsx
+++ livraria-virtual/src/pages/Book_Details/Book_Details.tsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import api from "../../services/api";
+import BookCardProps from "../../Interfaces/BookCardProps";
+import s from './Book_Details.module.css'
 
 export default function Book_Details() {
+    
+    var [book, setBook] = useState<BookCardProps | null>(null)
+    var bookId = useParams().bookId;
+    useEffect(() => {
+        api.get(`livros/${bookId}`)
+            .then(response => {
+                setBook(response.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
+    
+    if(!book){
+        return <div><h1>Livro não encontrado</h1></div>
+    }
+
 
     return (
-        <div>
-            <h1>Book_Details</h1>    
+        <div className={s.container}>
+            <div className={s.back_div}>
+                <Link to={`/home`} className={s.back_link}>{"< Detalhes do Livro"}</Link>
+            </div>
+            <div className={s.book_div_content}>
+                <div className={s.capa_div}>
+                    <img className={s.capa} src={book.capa} alt="" />
+                </div>
+                <div className={s.text_div}>
+                    <h1>{book.titulo}</h1>
+                    <p className={s.autor}>{book.autor}</p>
+                    <div className={s.sinopse_div}>
+                        <h2>Sinopse</h2>
+                        <p className={s.sinopse}>{book.sinopse}</p>
+                    </div>
+                    <div className={s.btn_div}>
+                        <button className={s.carrinho_btn}><p className={s.btn_text}>{`R$ ${book.preco}                   Adicionar ao carrinho`}</p></button>
+                    </div> 
+                </div>
+            </div>
+             
         </div>
     )
 
