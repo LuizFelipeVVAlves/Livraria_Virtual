b1504ed33bf196d55b6fac4004cae1263deca031
LuizFelipeVVAlves <lfvilaverde@gmail.com>
2025-03-02 18:18:01 -0300

    NOVIDADE
    - Adicionei filtro de busca por título

--- livraria-virtual/src/pages/Genders_Page/Genders_Page.tsx
+++ livraria-virtual/src/pages/Genders_Page/Genders_Page.tsx
@@ -5,15 +5,14 @@ import BookCardProps from "../../Interfaces/BookCardProps";
 import BookCard from "../../components/BookCard/bookCard";
 import s from "./Genders_Page.module.css"
 import Type from "../../Interfaces/Type";
-
+import Lupa from "../../assets/Lupa.svg"
 
 export default function Genders_Page() {
-    
     var [books, setBooks] = useState<BookCardProps[]>([])
+    var [searchTerm, setSearchTerm] = useState<string>("")
 
     var genero = useParams().genderName;
     useEffect(() => {
-
         api.get(`livros?genero=${genero}`)
             .then(response => {
                 console.log(response.data)
@@ -22,46 +21,42 @@ export default function Genders_Page() {
             .catch(error => {
                 console.log(error)
             })
+    }, [genero])
 
+    var filteredBooks = books.filter(book =>
+        book.titulo.toLowerCase().includes(searchTerm.toLowerCase())
+    )
 
-    }, [])
-
-    
     return (
-
         <div className={s.container}>
-            <div>
-                Busca
+            <div className={s.search_div}>
+                <input
+                    type="text"
+                    placeholder= "Pesquisar por título"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className={s.search_input}
+                />
             </div>
             <div className={s.back_div}>
                 <Link to={`/home`} className={s.back_link}>{`<  ${genero}`}</Link>
             </div>
-            
             <div className={s.books_container}>
-                {books.map((book: BookCardProps) => {
+                {filteredBooks.map((book: BookCardProps) => {    
                     return (
-                        <div className={s.book_div}>
+                        <div className={s.book_div} key={book.id}>
                             <BookCard autor={book.autor} 
                                 capa={book.capa} 
                                 genero={book.genero} 
                                 id={book.id} preco={book.preco} 
                                 sinopse={book.sinopse} 
                                 titulo={book.titulo} 
-                                key={book.id}
                                 type={Type.large}
-                                >
-
-                            </BookCard>
+                            />
                         </div>
                     )
                 })}
             </div>
-            
         </div>
-
     )
-
-
-
 }
-        
